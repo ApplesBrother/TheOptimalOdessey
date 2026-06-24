ceecc28931520698b3cdb71bfad5fd5db8ea36ed
ApplesBrother <106483090+ApplesBrother@users.noreply.github.com>
2026-06-24 16:18:35 +0530

    driver.cpp
    changed driver.cpp

--- Week 3/driver.cpp
+++ Week 3/driver.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-#include<algorithm>
-#include<nlohmann/json.hpp>
+#include<bits/tdc++.h>
+#include"json.hpp"
 #include<chrono>
+#include"Graph.h"
+#define INF 1000000000
+using namespace std;
+using namespace std::chrono;
+using json=nlohmann::ordered_json;
 
 int main(int argc, char* argv[]){
 
@@ -24,27 +26,20 @@ int main(int argc, char* argv[]){
     }
 
     nlohmann::json graph_json;
-    file1 >> graph_json; // reading the graph_json file into json 
+    file1 >> graph_json;
+    Graph map(graph_json);
 
-	/*!!! Need changes here !!!*/ 
-	
-	// Create a Graph class that takes the json input
-	// and stores the required graph data structures.
-	// Remember to include the header file containing the class.
-	// Uncomment the line below after implementing the class.
-	// Graph map(graph_json);
-
-    std::ifstream file2(query_json_file);   
+    std::ifstream file2(query_json_file);
 
     if (!file2.is_open()) {
         std::cerr << "Error: Could not open " << query_json_file << '\n';
         return 1;
     }
 
-    nlohmann::json query_json;
-    file2 >> query_json; // reading the query_json file into json object
+    json query_json;
+    file2 >> query_json;
 
-    nlohmann::json output_json;
+    json output_json;
 
     output_json["meta"] = {{"id", query_json["meta"]["id"]}};
     output_json["results"] = nlohmann::json::array();
@@ -54,20 +49,68 @@ int main(int argc, char* argv[]){
     for(auto event : query_json["events"]){
 
         type = event["type"];
-
-		/* Refer to the sample code below */
-        /*
-        if(type == "remove_edge") {
-            int edge_id = event["edge_id"];
-            bool b = remove_edge(map, edge_id);
-            nlohmann::json out;
-            out["id"] = event["id"];
-            out["done"] = b;
-            std::cout << out.dump(4) << std::endl;
-            output_json["results"].push_back(out);
+        if(type=="find_path"){
+                string b[]={"dijkstra","astar_euclidean","astar_manhattan"};
+                json result;
+                result["id"]=event["id"];
+                const int start=int(event["start"]["y"])*map.m+int(event["start"]["x"]);
+                const int stop=int(event["goal"]["y"])*map.m+int(event["goal"]["x"]);
+                vector<vector<float>>  h(3,vector<float>(map.m*map.n,0));
+                int ms=int(event["start"]["x"]),ns=int(event["start"]["y"]);
+                int me=int(event["goal"]["x"]),ne=int(event["goal"]["y"]);
+                for(int i=0;i<map.m*map.n;i++){
+                        int m=i%map.m,n=i/map.m;
+                        h[1][i]=sqrt((m-me)*(m-me)+(n-ne)*(n-ne));
+                        h[2][i]=abs(m-me)+abs(n-ne);
+                }
+                for(int i=0;i<3;i++){
+                        auto begin=high_resolution_clock::now();
+                        vector<bool> complete(map.m*map.n,false);
+                        vector<float> distance(map.m*map.n,INF);
+                        priority_queue<pair<float,int>> q;
+                        vector<Node> parent(map.m*map.n);
+                        distance[start]=0;
+                        int count=0;
+                        int len=0;
+                        q.push({0,start});
+                        while(!q.empty()){
+                                Node s=q.top().second;
+                                q.pop();
+                                if(complete[s.pos]){
+                                        continue;
+                                }
+                                for(auto k:map.get_neighbors(s)){
+                                        if(complete[k.pos]){
+                                                continue;
+                                        }
+                                        if(distance[k.pos]>distance[s.pos]+1){
+                                                distance[k.pos]=distance[s.pos]+1;
+                                                q.push({-distance[k.pos]-h[i][k.pos],k.pos});
+                                                parent[k.pos]=s;
+                                        }
+                                }
+                                complete[s.pos]=true;
+                                count++;
+                                if(s.pos==stop){
+                                        break;
+                                }
+                        }
+                        if(complete[stop]){
+                                int s=stop;
+                                while(s!=start){
+                                        len++;
+                                        s=parent[s].pos;
+                                }
+                                auto end=high_resolution_clock::now();
+                                auto t=duration<double,milli>(end-begin);
+                                result[b[i]]={{"path_found",true},{"path_length",len},{"nodes_explored",count},{"time_ms",t.count()}};
+                                continue;
+                        }
+                        result[b[i]]={{"path_found",false}};
+                }
+                output_json["results"].push_back(result);
         }
-        */
     }
-
+    ofstream(output_file)<<output_json.dump(4);
     return 0;
 }
